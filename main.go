24874ea08e7043241873a3f0f6e9c8e92c22113b
AndreiNanescu <91884910+AndreiNanescu@users.noreply.github.com>
2022-10-15 23:48:57 +0300

    Created an app greeting message

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("yo")
+	const eventName = "Test name"
+	const tickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to the", eventName, "booking application.")
+	fmt.Println("We have a total of", tickets, "of wich", remainingTickets, "are remaining.")
 }
